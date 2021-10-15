8a1d2e45ee92813cc6c719861658666257949093
likki0207 <68856803+likki0207@users.noreply.github.com>
2021-10-15 12:00:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ end_date = st.sidebar.date_input("End Date", date.today())
 # Specifying the data interval for which you want to visualize the plots
 data_interval = st.sidebar.selectbox(
     "Data Interval",
-    ("1m","2m","5m","15m","30m","60m","90m","1h","1d","5d","1wk","1mo","3mo",),
+    ("15m","30m","60m","90m","1h","1d","5d","1wk","1mo","3mo",),
 )
 
 symbol_crypto = crypto_mapping[crypto_option]
